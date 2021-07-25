84b7a4984a1b74f6287c4dfd9005a2709c6b914c
patsukaa <aezhik@mail.ru>
2021-07-25 15:54:03 +0300

    init

---
+++ src/main/java/ru/study/otusspringboot/OtusSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.study.otusspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtusSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/study/otusspringboot/OtusSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.study.otusspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
